afa2a5de65fa9f5e77b1fcdb02fdf59b0a0dc4d3
GGSMNandathilaka <ggsmnandathilaka>
2019-05-20 12:20:13 +0530

    Passing event araguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -23,7 +23,8 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -32,7 +33,7 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
