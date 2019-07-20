8b3d219c6ea7c0b216d1bfd54f5108f305dbd7fa
GGSMNandathilaka <ggsmnandathilaka>
2019-07-20 16:48:26 +0530

    specifying children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,60 +1,13 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.handleIncrement({ id: 1 })}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <div>
-          {this.state.tags.length === 0 && "Please add a tag!"}
-          {this.renderTags()}
-        </div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
       </React.Fragment>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
