faadfb1c7b7250d910bab2765d8c96a278afba08
GGSMNandathilaka <ggsmnandathilaka>
2019-07-20 19:53:24 +0530

    passing event arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,7 +6,8 @@ class Counter extends Component {
     tags: []
   };
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -27,7 +28,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
