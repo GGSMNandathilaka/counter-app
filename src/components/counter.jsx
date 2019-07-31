ca3a60f29401783543d302d183a5b9956b321f2b
GGSMNandathilaka <ggsmnandathilaka>
2019-08-01 00:22:48 +0530

    updating state, single source of truth, removing the local state of controlled component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,38 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    tags: []
-  };
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.value + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>'There are no tags!'</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   render() {
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => {
+            this.props.onIncrement(this.props.counter);
+          }}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete(this.props.counter.id);
+          }}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -40,12 +27,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
